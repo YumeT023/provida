dc5e51597a81ffb03bcb8818d18ed27578418ce6
Yume <hei.hajatiana@gmail.com>
2024-03-25 11:20:06 +0300

    chore: remove core package

--- packages/core/__tests__/sum.spec.ts
+++
@@ -1,9 +0,0 @@
-import {describe, expect, it} from "vitest";
-import {sum} from "../src";
-
-describe("sum", () => {
-  it("should sum two number", () => {
-    const result = sum(2, 3);
-    expect(result).to.eq(5);
-  });
-});
