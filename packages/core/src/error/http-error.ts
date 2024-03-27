2f2f41d07a978a7b01455bb7d1580134ee0a5184
Yume <hei.hajatiana@gmail.com>
2024-03-27 19:18:38 +0300

    feat(core): add abstract http error

---
+++ packages/core/src/error/http-error.ts
@@ -0,0 +1,20 @@
+/**
+ * Clazz that represents an http error
+ * TODO: inspect if a move to provida/error is needed
+ */
+export class HttpError extends Error {
+  constructor(
+    public readonly message: string,
+    public readonly status: number,
+    public readonly body: string | null = null
+  ) {
+    super(message);
+    Object.setPrototypeOf(this, HttpError.prototype);
+    this.name = this.constructor.name;
+    if (typeof Error.captureStackTrace === "function") {
+      Error.captureStackTrace(this, this.constructor);
+    } else {
+      this.stack = new Error().stack;
+    }
+  }
+}
